9fc62217fe42911bba583b740053317ad035fbfc
tcmetzger <39711796+tcmetzger@users.noreply.github.com>
2022-11-19 14:46:54 -0800

    Add basic CLI tests

---
+++ raster_loader/raster_loader_cli/tests/test_cli.py
@@ -0,0 +1,45 @@
+# TBD can be removed once main package is available
+import sys
+
+sys.path.append("../")
+
+from click.testing import CliRunner
+from cli import raster_loader_cli
+
+
+def test_info():
+    """Test the info command."""
+    runner = CliRunner()
+    result = runner.invoke(raster_loader_cli, ["info"])
+    assert result.exit_code == 0
+    assert "Python version" in result.output
+    assert "Operating system" in result.output
+
+
+def test_raster():
+    """Test the raster command."""
+    runner = CliRunner()
+    result = runner.invoke(
+        raster_loader_cli, ["raster", "load", __file__, "test_table"]
+    )
+    assert result.exit_code == 0
+    assert __file__ in result.output
+
+
+def test_raster_invalid_filename():
+    """Test whether the raster command checks for invalid filename."""
+    runner = CliRunner()
+    result = runner.invoke(
+        raster_loader_cli, ["raster", "load", "invalid file name", "test_table"]
+    )
+    assert result.exit_code == 2
+    assert "Error" in result.output
+
+
+def test_raster_missing_input():
+    """Test whether the raster command always requires a file and table input."""
+    runner = CliRunner()
+    result = runner.invoke(raster_loader_cli, ["raster", "load", __file__])
+    print(result.output)
+    assert result.exit_code == 2
+    assert "Error: Missing argument" in result.output
