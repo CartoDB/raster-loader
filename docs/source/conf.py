8c58d36628104b0be30a251863b569aed83862a0
tcmetzger <39711796+tcmetzger@users.noreply.github.com>
2022-11-18 16:13:41 -0800

    Configure Sphinx and theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,26 +3,61 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import sphinx_rtd_theme
+import datetime as dt
+from os import popen
+from shutil import which
+
+
+def get_version() -> str:
+    """Use git to read version from most recent tag.
+
+    Returns:
+        str: Version number
+    """
+    release = "dev"
+    if which("git"):
+        release = popen("git describe --tags --always").read().strip()
+        if release and release[0] == "v":
+            release = release[1:]
+        else:
+            release = f"dev-{release}"
+
+    return release
+
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Carto Raster Loader'
-copyright = '2022, Carto'
-author = 'Carto'
-release = '0.1'
+project = "Raster Loader"
+copyright = f"{dt.datetime.now().year}, Carto"
+author = "Carto"
+release = version = get_version()
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx_rtd_theme",
+    "sphinx.ext.napoleon",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+html_logo = "_static/carto-logo.png"
+html_theme_options = {
+    "display_version": True,
+}
+html_context = {
+    "display_github": True,
+    "github_user": "CartoDB",
+    "github_repo": "raster-loader",
+    "github_version": "main/docs/",
+}
+html_favicon = "_static/carto-logo.png"
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,20 +1,8 @@
-.. Carto Raster Loader documentation master file, created by
-   sphinx-quickstart on Fri Nov 18 15:17:54 2022.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
+.. docs_index
 
-Welcome to Carto Raster Loader's documentation!
-===============================================
+Raster Loader
+=============
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
-
-
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
