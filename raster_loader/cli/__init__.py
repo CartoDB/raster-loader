8210e79a5e0e8f41ce44a3b6a5d6985a0f33aaff
giancastro <giancastrok@gmail.com>
2022-11-23 11:48:27 -0300

    add basic cli

---
+++ raster_loader/cli/__init__.py
@@ -0,0 +1,17 @@
+from pkg_resources import iter_entry_points as entry_points
+
+import click
+from click_plugins import with_plugins
+
+
+@with_plugins(cmd for cmd in list(entry_points("raster_loader.cli")))
+@click.group(context_settings=dict(help_option_names=["-h", "--help"]))
+def main(args=None):
+    """
+    carto command line interface.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
--- raster_loader/raster_loader_cli/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Command-line interface for the raster loader."""
-
-from raster_loader.raster_loader_cli.cli import raster_loader_cli
-
-__all__ = [
-    "raster_loader_cli",
-]
