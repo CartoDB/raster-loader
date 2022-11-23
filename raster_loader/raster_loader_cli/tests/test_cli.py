cc3901d0c1d40048fa85045fe054ec69d6cd1332
tcmetzger <39711796+tcmetzger@users.noreply.github.com>
2022-11-22 21:33:16 -0800

    Update CLI tests

--- raster_loader/raster_loader_cli/tests/test_cli.py
+++ raster_loader/raster_loader_cli/tests/test_cli.py
@@ -1,5 +1,15 @@
+import os
+
 from click.testing import CliRunner
+from unittest.mock import patch
+
+from raster_loader import RasterLoader
 from raster_loader.raster_loader_cli import raster_loader_cli
+from tests import mocks
+
+FIXTURE_FILE = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "../../../tests/fixtures/mosaic.tif")
+)
 
 
 def test_info():
@@ -12,17 +22,17 @@ def test_info():
     assert "Operating system" in result.output
 
 
-# TBD: We need to mock the raster_loader.upload.RasterLoader.to_bigquery method (the Google API)
-# for the following test to make sense
-# def test_raster():
+## Test TBD!
+# @patch.object(RasterLoader, "_bigquery_client", return_value=mocks.bigquery_client())
+# def test_raster(*args, **kwargs):
 #     """Test the raster command."""
 #     runner = CliRunner()
 #     result = runner.invoke(
-#         raster_loader_cli, ["raster", "load", __file__, "test_project.test_dataset.test_table"]
+#         raster_loader_cli,
+#         ["raster", "load", FIXTURE_FILE, "test_project.test_dataset.test_table"],
 #     )
 #     assert result.exit_code == 0
-#     assert __file__ in result.output
-
+#     assert FIXTURE_FILE in result.output
 
 def test_raster_invalid_filename():
     """Test whether the raster command checks for invalid filename."""
@@ -39,7 +49,7 @@ def test_raster_invalid_table():
     """Test whether the raster command checks for invalid tables."""
     runner = CliRunner()
     result = runner.invoke(
-        raster_loader_cli, ["raster", "load", __file__, "invalid_table"]
+        raster_loader_cli, ["raster", "load", FIXTURE_FILE, "invalid_table"]
     )
     assert result.exit_code == 2
     assert "Error" in result.output
@@ -48,7 +58,7 @@ def test_raster_invalid_table():
 def test_raster_missing_input():
     """Test whether the raster command always requires a file and table input."""
     runner = CliRunner()
-    result = runner.invoke(raster_loader_cli, ["raster", "load", __file__])
+    result = runner.invoke(raster_loader_cli, ["raster", "load", FIXTURE_FILE])
     print(result.output)
     assert result.exit_code == 2
     assert "Error: Missing argument" in result.output
