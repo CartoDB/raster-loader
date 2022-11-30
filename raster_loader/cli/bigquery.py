cf615da1c6d56c1cf04844dacb43a545812f307d
brendancol <brendan@makepath.com>
2022-11-30 14:00:16 -0500

    added temp upload2 command so we can compare approaches

--- raster_loader/cli/bigquery.py
+++ raster_loader/cli/bigquery.py
@@ -18,7 +18,7 @@ def bigquery(args=None):
 @click.option("--table", help="The name of the table.", required=True)
 @click.option("--dst_crs", help="The EPSG code of the destination CRS.", default=4326)
 @click.option(
-    "--chunk_size", help="The number of pixels to upload in each chunk.", default=100
+    "--chunk_size", help="The number of blocks to upload in each chunk.", default=100
 )
 def upload(file_path, project, dataset, table, dst_crs, chunk_size):
     click.echo("Uploading raster file to Google BigQuery")
@@ -33,4 +33,22 @@ def upload(file_path, project, dataset, table, dst_crs, chunk_size):
         table=table,
         chunk_size=chunk_size,
     )
+    click.echo("Raster file uploaded to Google BigQuery")
+
+
+@bigquery.command(help="Upload a raster file to Google BigQuery.")
+@click.option("--file_path", help="The path to the raster file.", required=True)
+@click.option("--project", help="The name of the Google Cloud project.", required=True)
+@click.option("--dataset", help="The name of the dataset.", required=True)
+@click.option("--table", help="The name of the table.", required=True)
+@click.option("--dst_crs", help="The EPSG code of the destination CRS.", default=4326)
+@click.option(
+    "--chunk_size", help="The number of blocks to upload in each chunk.", default=100
+)
+def upload2(file_path, project, dataset, table, dst_crs, chunk_size):
+
+    from raster_loader.io import rasterio_to_bigquery
+
+    click.echo("Uploading raster file to Google BigQuery")
+    rasterio_to_bigquery(file_path, table, dataset, project, chunk_size)
     click.echo("Raster file uploaded to Google BigQuery")
