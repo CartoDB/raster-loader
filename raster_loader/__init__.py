28d8106cb8077144364aaf40bf4c678c575708a8
brendancol <brendan@makepath.com>
2022-12-01 15:56:03 -0500

    fixed __init__.py

--- raster_loader/__init__.py
+++ raster_loader/__init__.py
@@ -1,9 +1,9 @@
 from raster_loader._version import __version__
-from raster_loader.upload import RasterLoader
-from raster_loader.errors import UploadError
+
+from raster_loader.io import rasterio_to_bigquery
+
 
 __all__ = [
     "__version__",
-    "RasterLoader",
-    "UploadError",
+    "rasterio_to_bigquery",
 ]
