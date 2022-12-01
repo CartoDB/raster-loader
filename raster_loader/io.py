510b5abc2c249f8d0a0e3c8a714b500bb6391ba8
brendancol <brendan@makepath.com>
2022-12-01 11:15:28 -0500

    added reproject and multiband support

--- raster_loader/io.py
+++ raster_loader/io.py
@@ -1,8 +1,12 @@
+import json
+
 from typing import Iterable
 
 from affine import Affine
 import numpy as np
 import pandas as pd
+
+import pyproj
 
 
 def array_to_record(
@@ -11,10 +15,13 @@ def array_to_record(
     row_off: int = 0,
     col_off: int = 0,
     value_field: str = "band_1",
+    crs: str = "EPSG:4326",
+    band: int = 1,
 ) -> dict:
 
     height, width = arr.shape
 
+    # verify if this is center or outer bounds corner
     lon_NW, lat_NW = geotransform * (col_off, row_off)
     lon_NE, lat_NE = geotransform * (col_off + width, row_off)
     lon_SE, lat_SE = geotransform * (col_off + width, row_off + height)
@@ -23,6 +30,16 @@ def array_to_record(
     # required to append dtype to value field name for storage
     dtype_str = str(arr.dtype)
     value_field = "_".join([value_field, dtype_str])
+
+    attrs = {
+        "band": band,
+        "value_field": value_field,
+        "dtype": dtype_str,
+        "crs": crs,
+        "gdal_transform": geotransform.to_gdal(),
+        "row_off": row_off,
+        "col_off": col_off,
+    }
 
     record = {
         "lat_NW": lat_NW,
@@ -35,10 +52,30 @@ def array_to_record(
         "lon_SW": lon_SW,
         "block_height": height,
         "block_width": width,
+        "attrs": json.dumps(attrs),
         value_field: arr.tobytes(),
     }
 
     return record
+
+
+def record_to_array(record: dict, value_field: str = "band_1_int8") -> np.ndarray:
+    """Convert a record to a numpy array."""
+
+    # determine dtype
+    try:
+        dtype_str = value_field.split("_")[-1]
+        dtype = np.dtype(dtype_str)
+    except ValueError:
+        raise ValueError(f"Invalid dtype: {dtype_str}")
+
+    # determine shape
+    shape = (record["block_height"], record["block_width"])
+
+    arr = np.frombuffer(record[value_field], dtype=dtype)
+    arr = arr.reshape(shape)
+
+    return arr
 
 
 def import_rasterio():
@@ -58,29 +95,108 @@ def import_rasterio():
         raise ImportError(msg)
 
 
-def rasterio_to_record(file_path: str, band: int = 1) -> dict:
+def _record_to_file(
+    record: dict,
+    file_path: str,
+    value_field: str = "band_1_int8",
+    driver: str = "GTiff",
+) -> None:
+    """Write a record to a file."""
+
+    rasterio = import_rasterio()
+
+    arr = record_to_array(record, value_field)
+
+    h, w = arr.shape[-2:]
+
+    xrange = (record["lon_NW"], record["lon_NE"])
+    yrange = (record["lat_SW"], record["lat_NW"])
+
+    transform = rasterio.transform.from_bounds(*xrange, *yrange, w, h)
+
+    with rasterio.open(
+        file_path,
+        "w",
+        driver=driver,
+        height=h,
+        width=w,
+        count=1,
+        dtype=arr.dtype,
+        transform=transform,
+    ) as dst:
+        dst.write(arr, 1)
+        print("Wrote to {}".format(file_path))
+
+
+def bigquery_to_file(
+    table_id: str,
+    dataset_id: str,
+    project_id: str,
+    file_path: str,
+    driver: str = "GTiff",
+):
+    """Write BigQuery Raster to File"""
+    import os
+
+    path, ext = os.path.splitext(file_path)
+
+    df = bigquery_to_records(table_id, dataset_id, project_id)
+
+    for i, record in df.iterrows():
+        out_path = path + "_part" + str(i) + ext
+        _record_to_file(record, out_path, driver=driver)
+
+
+def rasterio_to_record(file_path: str, band: int = 1, input_crs: str = None) -> dict:
     """Open a raster file with rasterio."""
     rasterio = import_rasterio()
 
     with rasterio.open(file_path) as raster_dataset:
+
+        if input_crs is None:
+            input_crs = raster_dataset.crs.to_string()
+
         return array_to_record(
             raster_dataset.read(band),
             raster_dataset.transform,
+            crs=input_crs,
+            band=band,
         )
 
 
-def rasterio_windows_to_records(file_path: str, band: int = 1) -> Iterable:
+def rasterio_windows_to_records(
+    file_path: str, band: int = 1, input_crs: str = None
+) -> Iterable:
     """Open a raster file with rasterio."""
     rasterio = import_rasterio()
 
     with rasterio.open(file_path) as raster_dataset:
+
+        raster_crs = raster_dataset.crs.to_string()
+
+        if input_crs is None:
+            input_crs = raster_crs
+
+        elif input_crs != raster_crs:
+            print(f"WARNING: Input CRS({input_crs}) != raster CRS({raster_crs}).")
+
+        if not input_crs:
+            raise ValueError("Unable to find valid input_crs.")
+
         for _, window in raster_dataset.block_windows():
-            yield array_to_record(
+            rec = array_to_record(
                 raster_dataset.read(band, window=window),
                 raster_dataset.transform,
                 window.row_off,
                 window.col_off,
+                crs=input_crs,
+                band=band,
             )
+
+            if input_crs.upper() != "EPSG:4326":
+                rec = reproject_record(rec, input_crs, "EPSG:4326")
+
+            yield rec
 
 
 def import_bigquery():
@@ -115,17 +231,58 @@ def records_to_bigquery(
     client.load_table_from_dataframe(data_df, f"{project_id}.{dataset_id}.{table_id}")
 
 
+def bigquery_to_records(
+    table_id: str, dataset_id: str, project_id: str, limit=10
+) -> pd.DataFrame:
+    """Read a BigQuery table into a records pandas.DataFrame."""
+    bigquery = import_bigquery()
+
+    client = bigquery.Client(project=project_id)
+
+    query = f"SELECT * FROM `{project_id}.{dataset_id}.{table_id}` LIMIT {limit}"
+
+    return client.query(query).result().to_dataframe()
+
+
+def reproject_record(record: dict, src_crs: str, dst_crs: str = "EPSG:4326") -> dict:
+    """Inplace reproject the bounds (lon_NW, lat_NW, etc.) of a record."""
+
+    rasterio = import_rasterio()
+
+    src_crs = rasterio.crs.CRS.from_string(src_crs)
+    dst_crs = rasterio.crs.CRS.from_string(dst_crs)
+
+    for lon_col, lat_col in [
+        ("lon_NW", "lat_NW"),
+        ("lon_NE", "lat_NE"),
+        ("lon_SW", "lat_SW"),
+        ("lon_SE", "lat_SE"),
+    ]:
+
+        x, y = pyproj.transform(src_crs, dst_crs, record[lon_col], record[lat_col])
+        record[lon_col] = x
+        record[lat_col] = y
+
+    return record
+
+
 def rasterio_to_bigquery(
     file_path: str,
     table_id: str,
     dataset_id: str,
     project_id: str,
+    band: int = 1,
     chunk_size: int = None,
+    input_crs: int = None,
 ):
+
+    if isinstance(input_crs, int):
+        input_crs = "EPSG:{}".format(input_crs)
+
     """Write a raster file to a BigQuery table."""
     print("Loading raster file to BigQuery...")
 
-    records_gen = rasterio_windows_to_records(file_path)
+    records_gen = rasterio_windows_to_records(file_path, band, input_crs)
 
     if chunk_size is None:
         records_to_bigquery(records_gen, table_id, dataset_id, project_id)
@@ -142,61 +299,3 @@ def rasterio_to_bigquery(
             records_to_bigquery(records, table_id, dataset_id, project_id)
 
     print("Done.")
-
-
-'''
-FUTURE IDEAS:
-
-def import_redshift():
-    try:
-        import psycopg2
-    except ImportError:
-
-        msg = ("Psycopg2 is not installed.\n"
-               "Please install Psycopg2 to use this function.\n"
-               "See https://pypi.org/project/psycopg2/\n"
-               "for installation instructions.\n"
-               "Alternatively, run `pip install psycopg2` to install from pypi.")
-        raise ImportError(msg)
-
-def record_to_redshift(record: dict, table_name: str, schema_name: str, conn_str: str):
-    """Write a record to a Redshift table.
-    """
-    import_redshift()
-
-    with psycopg2.connect(conn_str) as conn:
-        with conn.cursor() as cur:
-            columns = ', '.join(record.keys())
-            values = ', '.join([f"'{value}'" for value in record.values()])
-            sql = f"INSERT INTO {schema_name}.{table_name} ({columns}) VALUES ({values})"  # noqa
-            cur.execute(sql)
-            conn.commit()
-
-
-def import_snowflake():
-    try:
-        import snowflake.connector
-    except ImportError:
-
-        msg = ("Snowflake is not installed.\n"
-               "Please install Snowflake to use this function.\n"
-               "See https://docs.snowflake.com/en/user-guide/python-connector.html\n"
-               "for installation instructions.\n"
-               "Alternatively, run `pip install snowflake-connector-python` to install from pypi.")
-        raise ImportError(msg)
-
-
-def record_to_snowflake(record: dict, table_name: str, schema_name: str, conn_str: str):
-    """Write a record to a Snowflake table.
-    """
-    import_snowflake()
-
-    with snowflake.connector.connect(**conn_str) as conn:
-        with conn.cursor() as cur:
-            columns = ', '.join(record.keys())
-            values = ', '.join([f"'{value}'" for value in record.values()])
-            sql = f"INSERT INTO {schema_name}.{table_name} ({columns}) VALUES ({values})"
-            cur.execute(sql)
-            conn.commit()
-
-'''
