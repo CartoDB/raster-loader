b566a90cca5c77dbfc0e678970e26eb7086dd029
brendancol <brendan@makepath.com>
2022-12-01 15:52:39 -0500

    added older tests as comment in test_io.py

--- raster_loader/tests/test_io.py
+++ raster_loader/tests/test_io.py
@@ -150,3 +150,49 @@ def test_bigquery_to_records():
     for c in test_cols:
         print("Testing column: {}".format(c))
         assert records1[c].equals(records2[c])
+
+
+# OLDER TESTS
+"""
+@patch.object(RasterLoader, "_bigquery_client", return_value=mocks.bigquery_client())
+def test_upload_to_bigquery_successful(*args, **kwargs):
+    raster_loader = RasterLoader(file_path="raster_loader/tests/fixtures/mosaic.tif")
+
+    raster_loader.to_bigquery(
+        project="mock_project",
+        dataset="mock_dataset",
+        table="raster_data",
+    )
+
+
+@patch.object(
+    RasterLoader,
+    "_bigquery_client",
+    side_effect=errors.ClientError,
+)
+def test_upload_to_bigquery_unsuccessful_client_error(*args, **kwargs):
+    raster_loader = RasterLoader(file_path="raster_loader/tests/fixtures/mosaic.tif")
+
+    with pytest.raises(errors.ClientError):
+        raster_loader.to_bigquery(
+            project="mock_project",
+            dataset="mock_dataset",
+            table="raster_data",
+        )
+
+
+@patch.object(
+    RasterLoader,
+    "_bigquery_client",
+    return_value=mocks.bigquery_client(load_error=True),
+)
+def test_upload_to_bigquery_unsuccessful_load_error(*args, **kwargs):
+    raster_loader = RasterLoader(file_path="raster_loader/tests/fixtures/mosaic.tif")
+
+    with pytest.raises(errors.UploadError):
+        raster_loader.to_bigquery(
+            project="mock_project",
+            dataset="mock_dataset",
+            table="raster_data",
+        )
+"""
