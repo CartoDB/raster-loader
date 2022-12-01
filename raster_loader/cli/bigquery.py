6431d70120d145ff08261044524eed8c8a559f58
brendancol <brendan@makepath.com>
2022-12-01 11:13:01 -0500

    refactored des_crs vs. src_crs logic, added default table name if none supplied

--- raster_loader/cli/bigquery.py
+++ raster_loader/cli/bigquery.py
@@ -1,3 +1,6 @@
+import os
+import uuid
+
 import click
 
 from raster_loader import RasterLoader
@@ -16,15 +19,15 @@ def bigquery(args=None):
 @click.option("--project", help="The name of the Google Cloud project.", required=True)
 @click.option("--dataset", help="The name of the dataset.", required=True)
 @click.option("--table", help="The name of the table.", required=True)
-@click.option("--dst_crs", help="The EPSG code of the destination CRS.", default=4326)
+@click.option("--input_crs", help="The EPSG code of the input dataset.", default=4326)
 @click.option(
     "--chunk_size", help="The number of blocks to upload in each chunk.", default=100
 )
-def upload(file_path, project, dataset, table, dst_crs, chunk_size):
+def upload(file_path, project, dataset, table, input_crs, chunk_size):
     click.echo("Uploading raster file to Google BigQuery")
     raster_loader = RasterLoader(
         file_path=file_path,
-        dst_crs=dst_crs,
+        input_crs=input_crs,
     )
 
     raster_loader.to_bigquery(
@@ -40,15 +43,44 @@ def upload(file_path, project, dataset, table, dst_crs, chunk_size):
 @click.option("--file_path", help="The path to the raster file.", required=True)
 @click.option("--project", help="The name of the Google Cloud project.", required=True)
 @click.option("--dataset", help="The name of the dataset.", required=True)
-@click.option("--table", help="The name of the table.", required=True)
-@click.option("--dst_crs", help="The EPSG code of the destination CRS.", default=4326)
+@click.option("--table", help="The name of the table.", default=None)
+@click.option("--band", help="Band within raster to upload.", default=1)
 @click.option(
     "--chunk_size", help="The number of blocks to upload in each chunk.", default=100
 )
-def upload2(file_path, project, dataset, table, dst_crs, chunk_size):
+@click.option(
+    "--input_crs", help="The EPSG code of the input raster's CRS.", default=None
+)
+def upload2(file_path, project, dataset, table, band, chunk_size, input_crs):
 
     from raster_loader.io import rasterio_to_bigquery
 
+    # TODO: implement hint
     click.echo("Uploading raster file to Google BigQuery")
-    rasterio_to_bigquery(file_path, table, dataset, project, chunk_size)
+    if table is None:
+        table = os.path.basename(file_path).split(".")[0]
+        table = "_".join([table, "band", str(band), str(uuid.uuid4())])
+
+    rasterio_to_bigquery(
+        file_path, table, dataset, project, band, chunk_size, input_crs
+    )
     click.echo("Raster file uploaded to Google BigQuery")
+
+
+@bigquery.command(help="Pull and Describe Table from BigQuery")
+@click.option("--project", help="The name of the Google Cloud project.", required=True)
+@click.option("--dataset", help="The name of the dataset.", required=True)
+@click.option("--table", help="The name of the table.", required=True)
+@click.option("--limit", help="Limit number of rows returned", default=10)
+def describe(project, dataset, table, limit):
+
+    from raster_loader.io import bigquery_to_records
+
+    df = bigquery_to_records(table, dataset, project, limit)
+    print(f"Table: {project}.{dataset}.{table}")
+    print(f"Number of rows: {len(df)}")
+    print(f"Number of columns: {len(df.columns)}")
+    print(f"Column names: {df.columns}")
+    print(f"Column types: {df.dtypes}")
+    print(f"Column descriptions: {df.describe()}")
+    print(f"Column head: {df.head()}")
