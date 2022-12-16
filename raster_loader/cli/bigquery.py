fd22f66d96220b869ca4ecae5477492b203d8a88
tcmetzger <39711796+tcmetzger@users.noreply.github.com>
2022-12-15 21:43:38 -0800

    Catch exceptions in CLI

--- raster_loader/cli/bigquery.py
+++ raster_loader/cli/bigquery.py
@@ -42,58 +42,60 @@ def upload(
     overwrite=False,
     test=False,
 ):
+    try:
+        from raster_loader.tests.mocks import bigquery_client
+        from raster_loader.io import import_bigquery
+        from raster_loader.io import rasterio_to_bigquery
+        from raster_loader.io import get_number_of_blocks
+        from raster_loader.io import print_band_information
+        from raster_loader.io import get_block_dims
 
-    from raster_loader.tests.mocks import bigquery_client
-    from raster_loader.io import import_bigquery
-    from raster_loader.io import rasterio_to_bigquery
-    from raster_loader.io import get_number_of_blocks
-    from raster_loader.io import print_band_information
-    from raster_loader.io import get_block_dims
+        # create default table name if not provided
+        if table is None:
+            table = os.path.basename(file_path).split(".")[0]
+            table = "_".join([table, "band", str(band), str(uuid.uuid4())])
 
-    # create default table name if not provided
-    if table is None:
-        table = os.path.basename(file_path).split(".")[0]
-        table = "_".join([table, "band", str(band), str(uuid.uuid4())])
+        # swap out BigQuery client for testing purposes
+        if test:
+            client = bigquery_client()
+        else:  # pragma: no cover
+            bigquery = import_bigquery()
+            client = bigquery.Client(project=project)
 
-    # swap out BigQuery client for testing purposes
-    if test:
-        client = bigquery_client()
-    else:  # pragma: no cover
-        bigquery = import_bigquery()
-        client = bigquery.Client(project=project)
+        # introspect raster file
+        num_blocks = get_number_of_blocks(file_path)
+        file_size_mb = os.path.getsize(file_path) / 1024 / 1024
 
-    # introspect raster file
-    num_blocks = get_number_of_blocks(file_path)
-    file_size_mb = os.path.getsize(file_path) / 1024 / 1024
+        click.echo("Preparing to upload raster file to BigQuery...")
+        click.echo("File Path: {}".format(file_path))
+        click.echo("File Size: {} MB".format(file_size_mb))
+        print_band_information(file_path)
+        click.echo("Source Band: {}".format(band))
+        click.echo("Number of Blocks: {}".format(num_blocks))
+        click.echo("Block Dims: {}".format(get_block_dims(file_path)))
+        click.echo("Project: {}".format(project))
+        click.echo("Dataset: {}".format(dataset))
+        click.echo("Table: {}".format(table))
+        click.echo("Number of Records Per BigQuery Append: {}".format(chunk_size))
+        click.echo("Input CRS: {}".format(input_crs))
 
-    click.echo("Preparing to upload raster file to BigQuery...")
-    click.echo("File Path: {}".format(file_path))
-    click.echo("File Size: {} MB".format(file_size_mb))
-    print_band_information(file_path)
-    click.echo("Source Band: {}".format(band))
-    click.echo("Number of Blocks: {}".format(num_blocks))
-    click.echo("Block Dims: {}".format(get_block_dims(file_path)))
-    click.echo("Project: {}".format(project))
-    click.echo("Dataset: {}".format(dataset))
-    click.echo("Table: {}".format(table))
-    click.echo("Number of Records Per BigQuery Append: {}".format(chunk_size))
-    click.echo("Input CRS: {}".format(input_crs))
+        click.echo("Uploading Raster to BigQuery")
 
-    click.echo("Uploading Raster to BigQuery")
+        rasterio_to_bigquery(
+            file_path,
+            table,
+            dataset,
+            project,
+            band,
+            chunk_size,
+            input_crs,
+            client=client,
+            overwrite=overwrite,
+        )
 
-    rasterio_to_bigquery(
-        file_path,
-        table,
-        dataset,
-        project,
-        band,
-        chunk_size,
-        input_crs,
-        client=client,
-        overwrite=overwrite,
-    )
-
-    click.echo("Raster file uploaded to Google BigQuery")
+        click.echo("Raster file uploaded to Google BigQuery")
+    except Exception as e:
+        click.echo(f"Error: {e}")
     return 0
 
 
@@ -104,13 +106,16 @@ def upload(
 @click.option("--limit", help="Limit number of rows returned", default=10)
 def describe(project, dataset, table, limit):
 
-    from raster_loader.io import bigquery_to_records
+    try:
+        from raster_loader.io import bigquery_to_records
 
-    df = bigquery_to_records(table, dataset, project, limit)
-    print(f"Table: {project}.{dataset}.{table}")
-    print(f"Number of rows: {len(df)}")
-    print(f"Number of columns: {len(df.columns)}")
-    print(f"Column names: {df.columns}")
-    print(f"Column types: {df.dtypes}")
-    print(f"Column descriptions: {df.describe()}")
-    print(f"Column head: {df.head()}")
+        df = bigquery_to_records(table, dataset, project, limit)
+        print(f"Table: {project}.{dataset}.{table}")
+        print(f"Number of rows: {len(df)}")
+        print(f"Number of columns: {len(df.columns)}")
+        print(f"Column names: {df.columns}")
+        print(f"Column types: {df.dtypes}")
+        print(f"Column descriptions: {df.describe()}")
+        print(f"Column head: {df.head()}")
+    except Exception as e:
+        click.echo(f"Error: {e}")
--- raster_loader/cli/info.py
+++ raster_loader/cli/info.py
@@ -10,11 +10,14 @@ import raster_loader
 
 @click.command(help="Display system information.")
 def info():
-    """Display system information."""
-    click.echo(f"Raster Loader version: {raster_loader.__version__}")
-    click.echo(f"Python version: {sys.version.split(' (')[0]}")
-    click.echo(f"Platform: {platform.platform()}")
-    click.echo(f"System version: {platform.system()} {platform.release()}")
-    click.echo(f"Machine: {platform.machine()}")
-    click.echo(f"Processor: {platform.processor()}")
-    click.echo(f"Architecture: {platform.architecture()[0]}")
+    try:
+        """Display system information."""
+        click.echo(f"Raster Loader version: {raster_loader.__version__}")
+        click.echo(f"Python version: {sys.version.split(' (')[0]}")
+        click.echo(f"Platform: {platform.platform()}")
+        click.echo(f"System version: {platform.system()} {platform.release()}")
+        click.echo(f"Machine: {platform.machine()}")
+        click.echo(f"Processor: {platform.processor()}")
+        click.echo(f"Architecture: {platform.architecture()[0]}")
+    except Exception as e:
+        click.echo(e)
