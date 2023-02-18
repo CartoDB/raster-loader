3aa7db0f1c84debaf83ecb13790f8318a0b3799d
Javier Goizueta <jgoizueta@gmail.com>
2023-02-18 01:04:27 +0100

    Fix raster boundary

--- raster_loader/io.py
+++ raster_loader/io.py
@@ -161,6 +161,30 @@ def section_geog(lat_N, lat_S, num_subdivisions, pseudo_planar, format):
     return polygon_geography([ring1, ring2], format, False, pseudo_planar)
 
 
+def coords_to_geography(coords, format, whole_earth, pseudo_planar):
+    # remove too-close coordinates cause they cause errors
+    # in BigQuery's ST_GEOGFROMGEOJSON
+    def are_too_close(point1, point2):
+        return (
+            math.fabs(point1[0] - point2[0]) <= 1e-13
+            and math.fabs(point1[1] - point2[1]) <= 1e-13
+        )
+
+    def filter_near_points(coords, point):
+        previous = None if not coords else coords[-1]
+        if not previous or not are_too_close(previous, point):
+            coords.append(point)
+        return coords
+
+    coords = functools.reduce(filter_near_points, coords, [])
+
+    # now let's make sure the initial and final points are exactly the same
+    if coords[0] != coords[-1]:
+        # replace the last point; never mind, it must be very close
+        coords[-1] = coords[0]
+    return polygon_geography([coords], format, not whole_earth, pseudo_planar)
+
+
 def block_geog(
     lon_NW,
     lat_NW,
@@ -202,27 +226,7 @@ def block_geog(
             + line_coords(lon_NW, lat_NW, lon_SW, lat_SW, lat_subdivisions, whole_earth)
         )
 
-    # remove too-close coordinates cause they cause errors
-    # in BigQuery's ST_GEOGFROMGEOJSON
-    def are_too_close(point1, point2):
-        return (
-            math.fabs(point1[0] - point2[0]) <= 1e-13
-            and math.fabs(point1[1] - point2[1]) <= 1e-13
-        )
-
-    def filter_near_points(coords, point):
-        previous = None if not coords else coords[-1]
-        if not previous or not are_too_close(previous, point):
-            coords.append(point)
-        return coords
-
-    coords = functools.reduce(filter_near_points, coords, [])
-
-    # now let's make sure the initial and final points are exactly the same
-    if coords[0] != coords[-1]:
-        # replace the last point; never mind, it must be very close
-        coords[-1] = coords[0]
-    return polygon_geography([coords], format, not whole_earth, pseudo_planar)
+    return coords_to_geography(coords, format, whole_earth, pseudo_planar)
 
 
 def pseudoplanar(x, y):
@@ -466,34 +470,36 @@ def table_columns(quadbin: bool, bands: List[str]) -> List[Tuple[str, str, str]]
 
 
 def raster_bounds(raster_dataset, transformer, pseudo_planar, format):
-    # compute whole bounds for metadata
-    width = raster_dataset.width
-    height = raster_dataset.height
-    lon_NW, lat_NW = transformer.transform(*(raster_dataset.transform * (0, 0)))
-    lon_NE, lat_NE = transformer.transform(*(raster_dataset.transform * (width, 0)))
-    lon_SW, lat_SW = transformer.transform(*(raster_dataset.transform * (0, height)))
-    lon_SE, lat_SE = transformer.transform(
-        *(raster_dataset.transform * (width, height))
-    )
+    raster_dataset.transform,
+    min_x = 0
+    min_y = 0
+    max_x = raster_dataset.width
+    max_y = raster_dataset.height
 
-    # use 1 subdivision in 64 pixels
-    lon_subdivisions = math.ceil(width / 64.0)
-    lat_subdivisions = math.ceil(height / 64.0)
-    return block_geog(
-        lon_NW,
-        lat_NW,
-        lon_NE,
-        lat_NE,
-        lon_SE,
-        lat_SE,
-        lon_SW,
-        lat_SW,
-        lon_subdivisions,
-        lat_subdivisions,
-        raster_orientation(raster_dataset),
-        pseudo_planar,
-        format,
+    x_subdivisions = math.ceil((max_x - min_x) / 64.0)
+    y_subdivisions = math.ceil((max_y - min_y) / 64.0)
+    pixel_coords = (
+        # SW -> SE
+        coord_range(min_x, max_y, max_x, max_y, x_subdivisions)
+        # SE -> NE
+        + coord_range(max_x, max_y, max_x, min_y, y_subdivisions)
+        # NE -> NW
+        + coord_range(max_x, min_y, min_x, min_y, x_subdivisions)
+        # NW -> SW
+        + coord_range(min_x, min_y, min_x, max_y, y_subdivisions)
     )
+    coords = [
+        transformer.transform(*(raster_dataset.transform * (x, y)))
+        for x, y in pixel_coords
+    ]
+    lon_NW, _ = transformer.transform(*(raster_dataset.transform * (min_x, min_y)))
+    lon_NE, _ = transformer.transform(*(raster_dataset.transform * (max_x, min_y)))
+    lon_SW, _ = transformer.transform(*(raster_dataset.transform * (min_x, max_y)))
+    lon_SE, _ = transformer.transform(*(raster_dataset.transform * (max_x, max_y)))
+    whole_earth = (
+        math.fabs(lon_NW - lon_NE) >= 360.0 and math.fabs(lon_SW - lon_SE) >= 360
+    )
+    return coords_to_geography(coords, format, whole_earth, pseudo_planar)
 
 
 def rasterio_windows_to_records(
