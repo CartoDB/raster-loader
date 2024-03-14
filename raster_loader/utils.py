e9ead80115aba3ed197feda2b971835b4f03dc11
thedae <thedae@gmail.com>
2024-03-14 20:12:23 +0100

    Trying again

--- raster_loader/utils.py
+++ raster_loader/utils.py
@@ -29,7 +29,7 @@ def batched(iterable, n):
     while batch := tuple(islice(it, n)):  # noqa
         yield batch
 
-def get_default_table_name(base_path: str, band: tuple[int]):
+def get_default_table_name(base_path: str, band):
     table = os.path.basename(base_path).split(".")[0]
     table = "_".join([table, "band", str(band), str(uuid.uuid4())])
     return re.sub(r"[^a-zA-Z0-9_-]", "_", table)
