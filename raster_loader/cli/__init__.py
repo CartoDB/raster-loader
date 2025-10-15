0f358828be5d83cf631c1d57832b67e03a62882c
juanrmn <jgonzalez@cartodb.com>
2025-10-15 09:18:52 +0200

    fix python init script

--- raster_loader/cli/__init__.py
+++ raster_loader/cli/__init__.py
@@ -1,10 +1,14 @@
-from pkg_resources import iter_entry_points as entry_points
+try:
+    from importlib.metadata import entry_points
+except ImportError:
+    # Fallback for Python < 3.8
+    from importlib_metadata import entry_points
 
 import click
 from click_plugins import with_plugins
 
 
-@with_plugins(cmd for cmd in list(entry_points("raster_loader.cli")))
+@with_plugins(cmd for cmd in list(entry_points(group="raster_loader.cli")))
 @click.group(context_settings=dict(help_option_names=["-h", "--help"]))
 def main(args=None):
     """
